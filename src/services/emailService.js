c8ca510ee8d2f5fc538c4ecfb3f1bc55b5a35545
Marco Fidel Castro Velasco <marco.castro@correounivalle.edu.co>
2026-03-30 20:54:41 -0500

    importando supabase en correos

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -1,5 +1,6 @@
 import { Resend } from 'resend';
 import PDFDocument from 'pdfkit';
+import { supabaseAdmin } from "../../supabaseAdminClient.js";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
